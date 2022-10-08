896e610179eddd3d0df58a35d7a1c7f36773fe11
Silvan Miller <silvanmiller@gmail.com>
2022-10-07 23:33:49 -0300

    atualizacao

--- src/components/LandinPageTourFinal/index.tsx
+++ src/components/LandinPageTourFinal/index.tsx
@@ -1,12 +1,31 @@
 import React from 'react';
+import BgDesktop from "../../assets/images/BG-Desktop.png";
+import Persona from '../../assets/images/Personagem2.png'
 import LogoG5 from '../../assets/images/Group 5.png';
-import { Container, Title, Logo } from './styles';
+import { Container, Title, Logo, Personagem, Fundoimg, StyledButton, SignupButton } from './styles';
 
 export const LandingPageTourFinal = () => {
     return (
         <Container>
+            <Fundoimg src={BgDesktop} />
+
             <Title>SEJA BEM-VINDO AO</Title>
+            <SignupButton>
+                <a href='/'>Já tenho cadastro</a>
+            </SignupButton>
+
+            
             <Logo src={LogoG5} alt="logo do cola ai" />
+
+
+            <Personagem src={Persona} />
+            <StyledButton >
+                <a href='/signup'>Quero me cadastrar</a>
+            </StyledButton>
+
+            
+            
         </Container>
+        
     )
 }
