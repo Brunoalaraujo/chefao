b73f085ae8b174e372cec3d74516ba2f03c93785
Brunoandradel.a <brunoandradel.a.dev@gmail.com>
2022-10-14 12:16:56 -0300

    Correcao ReadME

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -4,7 +4,7 @@ import Feed from './pages/Feed';
 import LandingPage from './pages/LandingPage';
 import LandingPageFinal from './pages/LandingPageFinal';
 import TourPage from './pages/TourPage'
-import PostFeeds from './pages/PostFeeds';
+// import PostFeeds from './pages/PostFeeds';
 
 import  {SignUpPage}  from './pages/SignUpPage';
 
@@ -20,7 +20,7 @@ export default function Routes(): JSX.Element {
                 <Route path="/LandingPage/2" element={<LandingPageFinal />} />
                 <Route path="/signup" element={<SignUpPage />} />
                 <Route path="/tour" element={<TourPage />} />
-                <Route path="/post" element={<PostFeeds/>} />
+                {/* <Route path="/post" element={<PostFeeds/>} /> */}
             </WapperRoute>
         </BrowserRouter>
     );
